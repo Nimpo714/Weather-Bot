e540ca064dfc77fe9b24d2ea882750812a35de34
Nimpo714 <vadimkakhurka5@gmail.com>
2025-11-22 14:27:05 +0400

    Update api_tokens.py

--- src/api_tokens.py
+++ src/api_tokens.py
@@ -1,9 +1,9 @@
 # -- Telegram bots
-telegram_bot = '8390533770:AAGYDbYtG_qoUeWeEniCgePB0gaAyxC94Dc'
+telegram_bot = 'your_telegram_token'
 
 # -- API Tokens
-open_weather_map_token = '8365843f400ed66471526f9b0ebe6b27'
-time_zone_token = 'hY7FKHLR4UJ6rGdmeUIMupY3NV_ph6htnglPh9H4OFY'
+open_weather_map_token = 'your_open_weather_map_token'
+time_zone_token = 'your_api_mapy_token'
 
 # -- Source for API tokens
 # https://home.openweathermap.org/api_keys -- open_weather_map_token
